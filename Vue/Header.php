ea5b51544bcabe25d7becb6bf42aa23bfbda7e9a
augiraudie <augustin.giraudier@etu.uca.fr>
2020-06-01 18:28:47 +0200

    icone dans la barre

--- Vue/Header.php
+++ Vue/Header.php
@@ -11,7 +11,7 @@ require "Vue/HTMLFunctions.php";
   </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <img class = "mw-3 mh-3" src="Public/LogoTwittus.png" alt="">
+      <img class = "w-25 h-25" src="Public/LogoTwittus.png" alt="">
       <a class="navbar-brand" href="/">Twittus</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
