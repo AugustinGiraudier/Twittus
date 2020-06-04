1b06c3f56550bf779b9879ef60fd7ed057e1e2a7
augiraudie <augustin.giraudier@etu.uca.fr>
2020-06-04 11:30:20 +0200

    message succes inscription

--- Controlleur/Home.php
+++ Controlleur/Home.php
@@ -43,7 +43,7 @@ else{
     if($Inscription->VerfifyInfos())
     {
       $Inscription->SetNewUser();
-      header("Location: ?step=connexion&succes=1");
+      header("Location: ?step=connexion&success=1");
       exit();
     }
   }
@@ -51,9 +51,5 @@ else{
   {
     $error = $e->getMessage();
   }
-}
-if(isset($GET['succes']))
-{
-  $success = "Inscription réussie avec succès !";
 }
 ?>
--- Vue/Home.php
+++ Vue/Home.php
@@ -7,13 +7,10 @@ require "../Vue/Header.php";
 
 <div class="container mt-5 border rounded">
   <?php if(isset($_GET['success'])):?> 
-    <div class="alert alert-success container mt-4 text-center">Vous avez été enregistré</div>
+    <div class="alert alert-success container mt-4 text-center">Vous avez été enregistré avec succès !</div>
   <?php endif?>
   <?php if($error):?> 
     <div class="alert alert-danger container mt-4 text-center"><?=$error?></div> 
-  <?php endif?>
-  <?php if($success):?> 
-    <div class="alert alert-success container mt-4 text-center"><?=$success?></div> 
   <?php endif?>
   <div class="row justify-content-md-center mt-5">
     <div class="col text-center mx-5">
